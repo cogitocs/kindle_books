7ffa26133723529e5c4d8fb053354f6a4d017d62
Ivan <shuapeng@ebay.com>
2022-08-28 20:04:09 -0700

    update target dir

--- scripts/mv_pdf_file.py
+++ scripts/mv_pdf_file.py
@@ -1,16 +1,19 @@
 import os
 import sys
 
-current_dir=os. getcwd() 
+current_dir=os.getcwd() 
+target_dir=sys.argv[2]
 def mv_files(dir):
     r = []
     for root, dirs, files in os.walk(dir):
         for name in files:
             if name.endswith(".pdf") or name.endswith(".py"): 
-                print(root +"/" + name)
-                print("[" + root.replace(current_dir,"") + "]" +"-"+name)
-                newfilename="[" + root.replace(current_dir,"") + "]" +"-"+name
-                os.system("cp "+ root +"/" + name +" "+current_dir+"category/" + newfilename)
+                print("source:"+root +"/" + name)
+                print("target file name:"+"[" + root.replace(current_dir+"/","") + "]" +"-"+name)
+                print("target dir:"+target_dir)
+                newfilename="[" + root.replace(current_dir+"/","") + "]" +"-"+name
+                os.system("cp "+ root +"/" + name +" "+target_dir+"/" + newfilename)
+
 
 def main():
     mv_files(sys.argv[1])
